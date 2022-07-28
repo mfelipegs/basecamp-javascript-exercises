6207d884d8f9992a1cfd64aab849a4dfb2d323b9
mfelipegs <matheusg.dasilva7@gmail.com>
2022-07-28 09:56:35 -0300

    Manipulando DOM finalizado

--- manipulando_dom/assets/js/script.js
+++ manipulando_dom/assets/js/script.js
@@ -1,17 +1,33 @@
 const bg = document.getElementsByTagName('body')[0];
-const buttonMode = document.getElementById('mode-selector');
+const buttonmode = document.getElementById('mode-selector');
 const footer = document.getElementsByTagName('footer')[0];
 const h1 = document.getElementById('page-title');
+const darkModeClass = 'dark-mode';
 
-buttonMode.addEventListener("click", changeMode);
+buttonmode.addEventListener("click", changeMode);
 
 function changeMode() {
     changeClass();
+    changeText();
 }
 
 function changeClass(){
-    buttonMode.classList.toggle('dark-mode');
-    h1.classList.toggle('dark-mode');
-    bg.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode');
+    buttonmode.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    bg.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText(){
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if(bg.classList.contains(darkModeClass)) {
+        buttonmode.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    buttonmode.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
 }
